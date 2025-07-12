7fb7d6ff2218313686239b2b614ee5add96899e8
Dmitry Sidorov-Biryukov <99192142+msbdd@users.noreply.github.com>
2025-07-12 13:03:50 +0200

    Fixed Linux fullscreen mode parameter;

--- setup_gui.py
+++ setup_gui.py
@@ -7,6 +7,7 @@ import obspy
 
 mpl_data_path = matplotlib.get_data_path()
 sys.setrecursionlimit(8000)
+sys.path.insert(0, 'src')
 
 obsipy_data_dir = os.path.join(
     os.path.dirname(obspy.__file__), "imaging", "data"
@@ -17,20 +18,22 @@ dist_info = next(Path(site_packages).glob("obspy-*.dist-info"))
 
 exe = Executable(
     script=os.path.join("src", "gui.py"),
-    base=None if sys.platform == "win32" else None,
+    base="Win32GUI" if sys.platform == "win32" else None,
     target_name="PPSD_Plot_GUI",
     icon="resources/icon.ico"
 )
 
 setup(
     name="PPSD_Plotter",
-    version="__VERSION__",
+    version="0.2.0",
     description="A simple PPSD plotting tool based on ObsPy",
     options={
         "build_exe": {
             "packages": ["obspy", "matplotlib", "yaml", "numpy",
                          "tqdm", "os", "pathlib"],
             "excludes": [],
+            "includes": ["localization_dicts",
+                         "ppsd_plotter_aux"],
             "include_files": [
                 (str(dist_info), f"lib/{dist_info.name}"),
                 (mpl_data_path, "lib/matplotlib/mpl-data"),
@@ -38,6 +41,7 @@ setup(
                 ("example", "example"),
                 ("LICENSE", "LICENSE"),
                 ("resources/icon.ico", "resources/icon.ico"),
+                ("resources/icon.png", "resources/icon.png"),
             ],
             "build_exe": "build/PPSD_Plotter_Windows_GUI"
         }
